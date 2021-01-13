9358128765728b44c8cb195df674961785f0f40b
leandro.godoy <leandro.godoy@acct.global>
2021-01-13 20:08:45 -0300

    change project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import DashBoard from "./pages/DashBoard/DashBoard";
+import List from "./pages/List/List";
+import SigIn from "./pages/SigIn/SigIn";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <List />
+    <SigIn />
+    <DashBoard />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/SigIn/SigIn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SigIn: React.FC = () => {
+  return (
+    <div>
+      <h1>SigIn</h1>
+    </div>
+  );
+};
+
+export default SigIn;
