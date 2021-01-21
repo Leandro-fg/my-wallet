72e947fbab02b7cbd8714f5cb69604af837e66b9
leandro.godoy <leandro.godoy@acct.global>
2021-01-20 23:35:18 -0300

    add color on theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import Layout from "./components/Layout";
 import dark from "./styles/themes/dark";
+import light from "./styles/themes/light";
 import GlobalStyles from "./styles/GlobalStyles";
 
 const src: React.FC = () => {
