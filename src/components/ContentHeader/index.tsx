60e49d188d84e7a2716f454f149b3c6a43616955
leandro.godoy <leandro.godoy@acct.global>
2021-02-15 18:01:15 -0300

    adjust dashboard component

--- src/components/ContentHeader/index.tsx
+++ src/components/ContentHeader/index.tsx
@@ -1,31 +1,26 @@
 import React from "react";
 
 import { Container, TitleContainer, Controllers } from "./styles";
-import SelectInput from "../../components/SelectInput";
 
 interface IContentHeaderProps {
   title: string;
   lineColor: string;
-  controllers: React.ReactNode;
+  children: React.ReactNode
 }
 
 const ContentHeader: React.FC<IContentHeaderProps> = ({
   title,
   lineColor,
-  controllers,
+  children,
 }) => {
-  const options = [
-    { value: "Rodrigo", label: "Rodrigo" },
-    { value: "Leandro", label: "Leandro" },
-    { value: "Sabrina", label: "Sabrina" },
-  ];
+  
 
   return (
     <Container>
       <TitleContainer>
         <h1>{title}</h1>
       </TitleContainer>
-      <Controllers>{controllers}</Controllers>
+      <Controllers>{children}</Controllers>
     </Container>
   );
 };
--- src/pages/DashBoard/DashBoard.tsx
+++ src/pages/DashBoard/DashBoard.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 
 import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
 
 import { Container } from "./styles";
 
 const DashBoard: React.FC = () => {
+  const options = [
+    { value: "Rodrigo", label: "Rodrigo" },
+    { value: "Leandro", label: "Leandro" },
+    { value: "Sabrina", label: "Sabrina" },
+  ];
   return (
     <div>
       <Container>
-        <ContentHeader />
+        <ContentHeader title="Dashboard" lineColor="F#fff">
+          <SelectInput options={options}/>
+        </ContentHeader>
       </Container>
     </div>
   );
