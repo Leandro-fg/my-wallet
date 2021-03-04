d614332fd3ad3963a1673ffaff7e9236352ce1cb
leandro.godoy <leandro.godoy@acct.global>
2021-03-04 18:41:56 -0300

    Routes apps

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 
-import Layout from "./components/Layout";
+import Routes from "./routes";
 import dark from "./styles/themes/dark";
 import light from "./styles/themes/light";
 
 const src: React.FC = () => {
   return (
     <ThemeProvider theme={dark}>
-      <Layout>
-      </Layout>
       <GlobalStyles />
+      <Routes/>
     </ThemeProvider>
   );
 };
