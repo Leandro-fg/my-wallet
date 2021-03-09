bcef7859be4c5b0ca64d6d3912a8ebc8dc67c44a
leandro.godoy <leandro.godoy@acct.global>
2021-03-09 20:47:11 -0300

    add years and make auto

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -71,25 +71,44 @@ const List: React.FC<IRouterParams> = ({ match }) => {
     { value: 12, label: "Dezembro" },
   ];
 
-  const years = [
+  /*  const years = [
     { value: 2021, label: 2021 },
     { value: 2020, label: 2020 },
     { value: 2019, label: 2019 },
     { value: 2018, label: 2018 },
   ];
+   */
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    listData.forEach((item) => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
+      }
+    });
+
+    return uniqueYears.map(year => {
+      return {
+        value: year,
+        label: year,
+      }
+    })
+
+  }, []);
 
   useEffect(() => {
-
     const filteredDate = listData.filter((item) => {
       const date = new Date(item.date);
       const month = String(date.getMonth() + 1);
       const year = String(date.getFullYear());
-      
+
       return month === monthSelected && year === yearSelected;
     });
 
     const formattedData = filteredDate.map((item) => {
-
       return {
         id: String(new Date().getTime()) + item.amount,
         description: item.description,
