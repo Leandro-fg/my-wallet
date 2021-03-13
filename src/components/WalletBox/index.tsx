e5a74fed7502824a08add7ed8a35e4a7de4ebdff
leandro.godoy <leandro.godoy@acct.global>
2021-03-13 13:15:15 -0300

    add WalletBox component

---
+++ src/components/WalletBox/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import dollarImg from "../../assets/dollar.svg";
+import arrowUpImg from "../../assets/arrow-up.svg";
+import arrowDownImg from "../../assets/arrow-down.svg";
+
+import { Container } from "./styles";
+
+interface IWalletBoxProps {
+  title: string;
+  amount: number;
+  footerLabel: string;
+  color: string;
+  icon: "dolar" | "arrowUp" | "arrowDown";
+}
+
+const walletBox: React.FC<IWalletBoxProps> = ({
+  title,
+  amount,
+  footerLabel,
+  color,
+  icon,
+}) => {
+  return (
+    <Container color={color}>
+      <span> {title}</span>
+      <h1>{amount}</h1>
+      <small>{footerLabel}</small>
+      <img src={dollarImg} alt={title} />
+    </Container>
+  );
+};
+
+export default walletBox;
