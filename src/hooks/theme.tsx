fdb9a89d8753b09e23cb75dd03dd354a6b65e548
leandro.godoy <leandro.godoy@acct.global>
2021-03-22 18:45:22 -0300

    add a localStorage coockie to save theme

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -28,13 +28,23 @@ interface ITheme {
 const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 
 const ThemeProvider: React.FC = ({ children }) => {
-  const [theme, setTheme] = useState<ITheme>(dark);
+  const [theme, setTheme] = useState<ITheme>(() => {
+    const savedTheme = localStorage.getItem("@my-wallet:theme");
+
+    if (savedTheme) {
+      return JSON.parse(savedTheme);
+    } else {
+      return dark;
+    }
+  });
 
   const toggleTheme = () => {
     if (theme.title === "dark") {
       setTheme(light);
+      localStorage.setItem("@my-wallet:theme", JSON.stringify(light));
     } else {
       setTheme(dark);
+      localStorage.setItem("@my-wallet:theme", JSON.stringify(dark));
     }
   };
   return (
